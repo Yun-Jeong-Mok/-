83c92f67d5d90245d3818eff0170605faed509d0
윤정목 <80142929+Yun-Jeong-Mok@users.noreply.github.com>
2021-06-03 20:34:11 +0900

    Add files via upload

---
+++ homepage/js/bgm.js
@@ -0,0 +1,35 @@
+// var audio1 = document.getElementById("audio1");
+// var audio2 = document.getElementById("audio2");
+// var playbtn1 = document.getElementById("playbtn1");
+// var playbtn2 = document.getElementById("playbtn2");
+// var count = 0;
+
+// function playStop1() {
+//     if (count == 0) {
+//         count = 1;
+//         audio1.play();
+//         playbtn1.innerHTML = "Pause";
+//     }
+//     else {
+//         count = 0;
+//         audio1.pause();
+//         audio1.volume = 0.5;
+//         playbtn1.innerHTML = "SoundTrack1";
+//     }
+// }
+
+// function playStop2() {
+//     if (count == 0) {
+//         count = 1;
+//         audio2.play();
+//         audio2.volume = 0.5;
+//         playbtn2.innerHTML = "Pause";
+//     }
+//     else {
+
+//         count = 0;
+//         audio2.pause();
+//         playbtn2.innerHTML = "SoundTrack2";
+//     }
+// }
+
